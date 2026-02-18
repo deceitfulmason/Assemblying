e678d266f2b9aab8a5072517e7eb4635c47de1ae
mason <deceitfulmason1987@gmail.com>
2026-02-18 17:47:38 -0300

    question2

---
+++ questao2.asm
@@ -0,0 +1,7 @@
+addi x9, x0, 50 # adiciona 50 + 0 a x9
+addi x10, x0, -50 # adiciona -50 + 0 a x10
+addi x17 x0, -15 # adiciona -15 a x17, como um valor temporario
+
+add x11, x9, x17 #adiciona o valor de x9(+50) e x17(-15) em x11
+
+sub x12, x0, x10
